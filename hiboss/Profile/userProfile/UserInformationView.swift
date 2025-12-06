78a1a17023ef3526731c89db4642f1a383d558b5
Raymond Xu <raymondxxu@outlook.com>
2025-12-06 12:47:54 -0800

    new page

--- hiboss/Profile/userProfile/UserInformationView.swift
+++ hiboss/Profile/userProfile/UserInformationView.swift
@@ -9,7 +9,41 @@ import SwiftUI
 
 struct UserInformationView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Basic information")
+                    .font(.custom("Futura", size: 28))
+                Spacer()
+            }
+            HStack {
+                Text("Avatar")
+                    .font(.custom("Futura", size: 17))
+                Spacer()
+                Image(systemName: "person.circle")
+                    .resizable()
+                    .frame(width: 37, height: 37)
+                Image(systemName: "camera.circle")
+                    .resizable()
+                    .frame(width: 37, height: 37)
+            }
+            Text("Name")
+                .font(.custom("Futura", size: 17))
+            HStack {
+                Text("please select your name")
+                    .font(.custom("PingFang", size: 17))
+                    .foregroundStyle(Color(hex: "#999999"))
+                    .padding(.leading, 18)
+                Spacer()
+                Image(systemName: "chevron.down")
+                    .padding(.trailing, 46)
+            }.padding(.vertical)
+                .background {
+                    RoundedRectangle(cornerRadius: 14)
+                        .fill(Color(hex: "#F4F6F8"))
+                }
+            Spacer()
+        }.padding([.top, .bottom])
+         .padding([.horizontal], 31)
     }
 }
 
